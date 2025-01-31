3c4147ddc3eff1dc1781d47ef02e86b5246524ba
wm-watson <wpwatson@uams.edu>
2025-01-31 13:38:17 -0600

    filepath

--- 1. Raw_Claims_and_Cleaning.R
+++ 1. Raw_Claims_and_Cleaning.R
@@ -1,11 +1,13 @@
 # 1. Raw Claims ----
 ## Raw Claims ----
 library(tidyverse)
+library(beepr)
 
-# Base Filepath
-filepath <- "/Users/williamwatson/Library/CloudStorage/OneDrive-UniversityofArkansasforMedicalSciences/Deductible_Project/Deductibles/Data/"
+#Filepath
+filepath <- "/Users/williamwatson/Library/CloudStorage/OneDrive-UniversityofArkansasforMedicalSciences/Deductible_Project/Deductibles/"
 
-# Read data by just adding filename to filepath
-data <- read.csv(paste0(filepath, "final.csv"))
-
+# Read data by adding filename to filepath
+final <- read.csv(paste0(filepath, "final.csv"),
+                  header = TRUE)
+beep(8)
 #Bring in df, unique_mem, and final
